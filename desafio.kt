7bff79209fafa85db9f7b52798a2789bb70e7da1
Felipe-byte22 <64445712+Felipe-byte22@users.noreply.github.com>
2023-09-02 18:55:42 -0300

    Update do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,44 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario(val nome: String) {
+    override fun toString(): String {
+        return "$nome"
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+class Curso(var nome: String, var duracao: String, var nivelCurso: Nivel) {
+    override fun toString(): String {
+        return "$nome, $duracao, $nivelCurso"
+    }
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Bootcamp(val nome: String, val cursos: List<Curso>) {
 
-    val inscritos = mutableListOf<Usuario>()
+    var inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(usuario: Usuario, cursos: Curso) {
+        inscritos.add(usuario)
+        println("$usuario foi matriculado no Bootcamp $nome | Curso: $cursos")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criando os usuários
+    val usuario1 = Usuario("Felipe")
+    val usuario2 = Usuario("Ana")
+    val usuario3 = Usuario("Renan")
+    
+    // Criando os cursos do Bootcamp
+    val curso1 = Curso("Versionamento de Código com Git e GitHub", "Duração: 2 horas", Nivel.BASICO)
+    val curso2 = Curso("Tratamento de Exceções em Kotlin", "Duração: 2 horas", Nivel.INTERMEDIARIO)
+    val curso3 = Curso("Ciclo de Vida Android ViewModel, LiveData e Coroutines", "Duração: 4 horas", Nivel.AVANCADO)
+    
+    // Criando o Bootcamp
+    val bootcamp = Bootcamp("Mobile Android com Kotlin", listOf(curso1, curso2, curso3))
+    
+    // Matriculando os usuários no Bootcamp
+    bootcamp.matricular(usuario1, curso1)
+    bootcamp.matricular(usuario2, curso2)
+    bootcamp.matricular(usuario3, curso3)
+    
 }
